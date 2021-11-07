0a7e3c50caff49716db7c4a0ac78777440beab0b
Matthew Cruz <mcruz772@mail.endicott.edu>
2021-11-06 20:20:21 -0400

    I added Navigation Code

---
+++ Navigation.js
@@ -0,0 +1,10 @@
+import { Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+
+const Navigation = () => {
+  
+  return <Text>Hello!!!!</Text>;
+
+};
+
+export default Navigation;
